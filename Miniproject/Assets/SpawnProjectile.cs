2ba7167d352786084d6882306a3e0481b446b1d0
Daniel Fridjonsson <Dani0223@hotmail.com>
2015-04-21 11:11:15 +0200

    Starting cleanup.

---
+++ Miniproject/Assets/SpawnProjectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            temp.transform.position = gameObject.transform.position;
+            temp.AddComponent<Rigidbody>();
+        }
+	}
+}
