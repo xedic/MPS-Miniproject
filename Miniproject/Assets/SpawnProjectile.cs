89bfd7e896507b330927f8277cd365f566dc5472
Dennis Nielsen <dennis@nobullshitgames.com>
2015-04-23 13:23:45 +0200

    BOOM

--- Miniproject/Assets/SpawnProjectile.cs
+++ Miniproject/Assets/SpawnProjectile.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class SpawnProjectile : MonoBehaviour {	
 	// Update is called once per frame
+
+    public GameObject bomb;
+
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             temp.transform.position = gameObject.transform.position;
             temp.AddComponent<Rigidbody>();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GameObject temp = Instantiate(bomb);
+            temp.transform.position = gameObject.transform.position;
         }
 	}
 }
---
+++ Miniproject/Assets/bomb.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bomb : MonoBehaviour {
+
+    public float radius = 10.0F;
+    public float power = 10.0F;
+    public GameObject explosionParticle;
+
+    void Start()
+    {
+    }
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        if (c.gameObject.tag != "Player")
+        {
+            foreach (Collider hit in colliders)
+            {
+                if (hit && hit.GetComponent<Rigidbody>())
+                    hit.GetComponent<Rigidbody>().AddExplosionForce(power, explosionPos, radius, 3.0F);
+
+            }
+            GameObject t = Instantiate(explosionParticle);
+            t.transform.position = gameObject.transform.position;
+            Destroy(gameObject);
+        }
+
+    }
+
+}
