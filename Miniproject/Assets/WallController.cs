9b63495b31baa85ca9c91b532a23e726ee1f8f56
Daniel Fridjonsson <Dani0223@hotmail.com>
2015-04-27 10:05:13 +0200

    Walls can now respawn with different type (keybind '.' and ',')

---
+++ Miniproject/Assets/WallController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour {
+
+    private WallBuilder wall;
+
+    void Start()
+    {
+        wall = GetComponent<WallBuilder>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Comma))
+        {
+            wall.type = WallBuilder.BrickType.BRICK;
+            wall.ResetWall();
+        }
+        else if (Input.GetKey(KeyCode.Period))
+        {
+            wall.type = WallBuilder.BrickType.STEEL;
+            wall.ResetWall();
+        }
+	}
+}
